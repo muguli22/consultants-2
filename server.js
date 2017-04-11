d2be4c8a236f1ebba30d4b4d3402f31f91095421
Ramon Schmidt <ramonroc@gmail.com>
2017-04-11 02:09:26 -0300

    Add express router and installing mongoose

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 'use strict';
 
-const express = require('express');
+// server.js
 
-// Constants
-const PORT = 8080;
+// BASE SETUP
+// =============================================================================
 
-// App
-const app = express();
-app.get('/', function (req, res) {
-    res.send('Hello world\n');
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
