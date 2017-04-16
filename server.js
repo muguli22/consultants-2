bd8ec9501c2c0e18e94161754190ad1bbbb2b898
Ramon Schmidt <ramonroc@gmail.com>
2017-04-16 18:00:59 -0300

    Babel working with a simple server

--- server.js
+++ server.js
@@ -1,55 +1,48 @@
-'use strict';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-// server.js
+const app = express();
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express = require('express');        // call express
-var app = express();                 // define our app using express
-var bodyParser = require('body-parser');
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+const myLogger = (req, res, next) => {
+    console.log("Logged");
+    next();
+}
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://ramon:ramon@ds159330.mlab.com:59330/consultant'); // connect to our database
+app.get('/', myLogger, (req, res) => res.send('Hello World!'));
 
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function () {
-    console.log('uhuuul connected');
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
 });
 
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-var bearRoutes = require('./app/routes/bear');
-// middleware to use for all requests
-router.use(function (req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
-    res.json({message: 'hooray! welcome to our api!'});
-});
-
-// REGISTER OUR ROUTES -------------------------------
-app.use('/api', [router, bearRoutes]);
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://ramon:ramon@ds159330.mlab.com:59330/consultant'); // connect to our database
+//
+// var db = mongoose.connection;
+//
+// db.on('error', console.error.bind(console, 'connection error:'));
+//
+// db.once('open', function () {
+//     console.log('uhuuul connected');
+// });
 
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+// var router = express.Router();              // get an instance of the express Router
+// var bearRoutes = require('./app/routes/bear');
+//
+// router.use(function (req, res, next) {
+//
+//     console.log('Something is happening.');
+//     next(); // make sure we go to the next routes and don't stop here
+// });
+//
+//
+// router.get('/', function (req, res) {
+//     res.json({message: 'hooray! welcome to our api!'});
+// });
+//
+//
+// app.use('/api', [router, bearRoutes]);
