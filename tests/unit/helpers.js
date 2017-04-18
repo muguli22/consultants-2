32e624ee8ea8eb10a566dde12620f22d119add92
Ramon Schmidt <ramonroc@gmail.com>
2017-04-18 11:00:52 -0300

    Added Unit tests battery and First controller resolved #5 resolved #6 resolved #4

---
+++ tests/unit/helpers.js
@@ -0,0 +1,7 @@
+/**
+ * Responsable for initialize the tests configurations
+ * without need to realize that configuration in each test case
+ */
+import chai from 'chai';
+
+global.expect = chai.expect;
