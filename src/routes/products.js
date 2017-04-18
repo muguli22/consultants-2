f3de067949f20d4d5a627d6938d802e30a3b4f26
Ramon Schmidt <ramonroc@gmail.com>
2017-04-18 11:08:26 -0300

    Refactoring products routes to use products controller

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,15 +1,11 @@
 import express from 'express';
+import ProductsController from '../controllers/products';
 
 const router = express.Router();
+const productsController = new ProductsController();
 
-router.get('/', (req, res) => res.send(
-    [
-        {
-            name: 'Default product',
-            description: 'product description',
-            price: 100
-        }
-    ]
-));
+router.get('/', (req, res) =>
+    productsController.get(req, res)
+);
 
 export default router;
